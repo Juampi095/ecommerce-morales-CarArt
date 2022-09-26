d91b1543f3a6815b6cc7f35176208fa8c3b3d883
Juampi095 <azarjpm15@gmail.com>
2022-09-26 20:57:58 -0300

    instale boostrap

---
+++ src/components/CartWidget.js
@@ -0,0 +1,8 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'; //Se importa lo que quiero usar en cada hoja 
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
+//Se instalan todos los paquetes de iconos para poder usarse correctamente
+function CartWidget(){ 
+    return(
+        <FontAwesomeIcon icon={faCartShopping} />
+    )
+}
